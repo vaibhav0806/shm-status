e11ae6719363bef1e97482dfb02b5712dc02a538
Vaibhav <vaibhav.p@xpuls.ai>
2024-06-10 23:33:36 +0530

    initialize express project

---
+++ index.ts
@@ -0,0 +1,12 @@
+const express = require('express');
+import { Request, Response } from "express";
+const app = express();
+const port = 3002;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, world');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
