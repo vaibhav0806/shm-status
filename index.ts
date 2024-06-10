1958dcff43ecf62208a193e5f036cf2e715a6077
Vaibhav <vaibhav.p@xpuls.ai>
2024-06-10 23:42:27 +0530

    initialize prometheus client

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 const express = require('express');
 import { Request, Response } from "express";
+const prometheus = require('prom-client');
 const app = express();
 const port = 3002;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, world');
+// const collectDefaultMetrics = prometheus.collectDefaultMetrics;
+// collectDefaultMetrics();
+
+app.get('/metrics', async (req: Request, res: Response) => {
+    const metrics = await prometheus.register.metrics();
+    res.set('Content-Type', prometheus.register.contentType);
+    res.end(metrics);
 })
 
 app.listen(port, () => {
